2dc65e16ec090ad2c036e25deb16cf78ded2589e
Ricardo Aragon <richard015ar@gmail.com>
2020-04-28 19:16:26 -0300

    pbd-3 - Adding ais-hits items / book cards

--- src/App.vue
+++ src/App.vue
@@ -22,11 +22,49 @@
 
           <div class="search-panel__results">
             <ais-search-box placeholder="Search here…" class="searchbox" />
-            <ais-hits>
-              <template slot="item" slot-scope="{ item }">
-                <h1><ais-highlight :hit="item" attribute="name" /></h1>
-                <p><ais-highlight :hit="item" attribute="description" /></p>
-              </template>
+            <ais-hits :transform-items="transformItems">
+              <div class="books" slot-scope="{ items }">
+                <ais-state-results>
+                  <template slot-scope="{ query, hits }">
+                    <p class="books-no-results" v-if="hits.length === 0">
+                      No results found matching <strong>{{query}}</strong>.
+                    </p>
+                  </template>
+                </ais-state-results>
+                <article
+                  v-for="item in items"
+                  :key="item.objectID"
+                  class="book"
+                >
+                  <div class="media-object" :style="`background-image: url('${item.image}');`">
+                  </div>
+                  <div class="media">
+                    <h2 class="media-title">
+                      {{item.name}}
+                    </h2>
+                    <div class="media-detail">
+                      <div class="media-row">
+                        <strong>Author(s): </strong>{{item.author}}
+                      </div>
+                      <div class="media-row">
+                        <strong>Editor(s): </strong>
+                      </div>
+                      <div class="media-row">
+                        <strong>Subject(s): </strong>
+                      </div>
+                      <div class="media-row">
+                        <strong>Publisher: </strong>{{item.publisher_name}}
+                      </div>
+                      <div class="media-row">
+                        <strong>Word Count: </strong>
+                      </div>
+                      <div class="media-row">
+                        <strong>Description: </strong>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </div>
             </ais-hits>
 
             <div class="pagination"><ais-pagination /></div>
@@ -41,7 +79,17 @@
 import algoliasearch from 'algoliasearch/lite';
 import 'instantsearch.css/themes/algolia-min.css';
 
+import './App.css';
+
 export default {
+  methods: {
+    transformItems(items) {
+      return items.map(item => ({
+        ...item,
+        author: item.author.join(', '),
+      }));
+    }
+  },
   data() {
     return {
       searchClient: algoliasearch(
@@ -53,73 +101,3 @@ export default {
   },
 };
 </script>
-
-<style>
-body,
-h1 {
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
-    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-}
-
-.header {
-  display: flex;
-  align-items: center;
-  min-height: 50px;
-  padding: 0.5rem 1rem;
-  background-image: linear-gradient(to right, #4dba87, #2f9088);
-  color: #fff;
-  margin-bottom: 1rem;
-}
-
-.header a {
-  color: #fff;
-  text-decoration: none;
-}
-
-.header-title {
-  font-size: 1.2rem;
-  font-weight: normal;
-}
-
-.header-title::after {
-  content: ' ▸ ';
-  padding: 0 0.5rem;
-}
-
-.header-subtitle {
-  font-size: 1.2rem;
-}
-
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 1rem;
-}
-
-.search-panel {
-  display: flex;
-}
-
-.search-panel__filters {
-  flex: 1;
-  margin-right: 1em;
-}
-
-.search-panel__results {
-  flex: 3;
-}
-
-.searchbox {
-  margin-bottom: 2rem;
-}
-
-.pagination {
-  margin: 2rem auto;
-  text-align: center;
-}
-</style>
---
+++ src/components/BookCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <ais-hits>
+        <div slot="item" slot-scope="{ item, index }">
+            {{ index }} - {{ item.name }}
+        </div>
+    </ais-hits>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    };
+</script>
