629104a20378b8b76c288f1ddb1ae3b7dc8da0a7
Ricardo Aragon <ricardo@pressbooks.com>
2020-07-13 15:39:57 -0300

    Trying with exclude filter action filtering the rest the other filters

--- src/components/filters/Licenses.vue
+++ src/components/filters/Licenses.vue
@@ -7,30 +7,42 @@
         <template v-slot:activator>
             <v-list-item-title>LICENSE</v-list-item-title>
         </template>
-        <ais-refinement-list
-            attribute="license_name"
-            :searchable="false"
-            operator="or"
-            :limit="5"
-            show-more
-            :show-more-limit="500"
-            :sort-by="['name:asc']"
-        >
-            <v-list-item
-                slot="item"
-                slot-scope="{ item, refine }"
+        <template>
+            <ais-refinement-list
+                attribute="license_name"
+                :searchable="false"
+                operator="or"
+                :limit="15"
+                :sort-by="['name:asc']"
             >
-                <v-list-item-action
-                    @click.prevent="refine(item.value)"
-                    :id="item.value.split(' ').join('-').toLowerCase()"
+                <div
+                    slot-scope="{
+                        items,
+                        refine
+                    }"
                 >
-                    <v-checkbox
-                        v-model="item.isRefined"
-                        :label="cleanLicense(item)"
-                    ></v-checkbox>
-                </v-list-item-action>
-            </v-list-item>
-        </ais-refinement-list>
+                    <v-list-item
+                        v-for="item in items" :key="item.value"
+                    >
+                        <v-list-item-content>
+                            {{ cleanLicense(item) }}
+                        </v-list-item-content>
+                        <v-list-item-action
+                            :id="item.value.split(' ').join('-').toLowerCase()"
+                        >
+                            <div>
+                                <v-btn icon @click="applyFilter(refine, items, item.value, false)">
+                                    <v-icon color="green">mdi-check</v-icon>
+                                </v-btn>
+                                <v-btn icon @click="applyFilter(refine, items, item.value, true)">
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                            </div>
+                        </v-list-item-action>
+                    </v-list-item>
+                </div>
+            </ais-refinement-list>
+        </template>
         <v-list-item>
             <ais-clear-refinements  :included-attributes="['license_name']">
                 <div slot-scope="{ canRefine, refine }">
@@ -50,6 +62,17 @@
     export default {
         name: "Licenses",
         methods: {
+            applyFilter(refine, items, itemValue, exclude) {
+                if (exclude) {
+                    items.forEach((i) => {
+                        if (i.value !== itemValue) {
+                            refine(i.value);
+                        }
+                    })
+                    return true;
+                }
+                refine(itemValue);
+            },
             cleanLicense(item) {
                 let parts = item.value.split(' (');
                 if(parts.length > 1) {
