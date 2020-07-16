4882bb2bf6de77927ca1c11cd1070a0a8e911c8b
ricardopressbooks <63249561+ricardopressbooks@users.noreply.github.com>
2020-07-16 11:26:35 -0300

    Adding search button + e2e test for the feature (#60)
    * Adding search button + e2e test for the feature
    
    * Adding responsive searchbox component

--- src/components/Searchbox.vue
+++ src/components/Searchbox.vue
@@ -3,15 +3,37 @@
             class="searchbox"
             v-slot="{ refine }"
     >
-        <h2 class="ais-SearchBox__text">Find a book:</h2>
-        <v-text-field
-                type="search"
-                v-model="stringSearch"
-                label="Search all book metadata"
-                @input="enableFilters(refine, stringSearch)"
-        >
-            <v-icon slot="prepend">mdi-magnify</v-icon>
-        </v-text-field>
+        <v-form @submit.prevent="enableFilters(refine, stringSearch)">
+            <v-row
+                justify="space-between"
+            >
+                <v-col
+                    cols="12"
+                    md="2"
+                >
+                    <h2 class="ais-SearchBox__text">Find a book:</h2>
+                </v-col>
+                <v-col
+                    md="9"
+                    cols="12"
+                >
+                    <v-text-field
+                            type="search"
+                            v-model="stringSearch"
+                            label="Search all book metadata"
+                    >
+                        <v-icon slot="prepend">mdi-magnify</v-icon>
+                    </v-text-field>
+                </v-col>
+                <v-col
+                    md="1"
+                    cols="12"
+                    align="center"
+                >
+                    <v-btn type="submit" id="search-button">Search</v-btn>
+                </v-col>
+            </v-row>
+        </v-form>
         <a href="https://www.algolia.com/" aria-label="Search by Algolia" target="_blank" class="filters__stats-algolia-logo"> </a>
     </ais-search-box>
 </template>
