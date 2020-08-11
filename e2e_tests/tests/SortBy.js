2575e60fc264cb6994882d6a0c6f36f7e5e756ea
Ricardo Aragon <ricardo@pressbooks.com>
2020-08-11 18:06:24 -0300

    Fixing lint issue

--- e2e_tests/tests/SortBy.js
+++ e2e_tests/tests/SortBy.js
@@ -9,19 +9,19 @@ module.exports = {
             .click('#sort-by-test_Pressbooks_directory_sort_by_wordCount')
             .pause(3000)
             .waitForElementVisible('.ais-Hits__books-book-wordcount')
-            .elements('css selector', '.ais-Hits__books-book', function(bookElement) {
+            .elements('css selector', '.ais-Hits__books-book', (bookElement) => {
                 let previousWC = 0, elementId;
                 bookElement.value.forEach((v) => {
                     if (!v.hasOwnProperty('ELEMENT')) {
                         v.ELEMENT = Object.values(v)[0];
                     }
-                    browser.elementIdElement(v.ELEMENT, 'css selector', '.ais-Hits__books-book-wordcount', function (elem) {
+                    browser.elementIdElement(v.ELEMENT, 'css selector', '.ais-Hits__books-book-wordcount', (elem) => {
                         if (!elem.value.hasOwnProperty('ELEMENT')) {
                             elem.value.ELEMENT = Object.values(elem.value)[0];
                         }
                         elementId = elem.value.ELEMENT;
                     }).perform((done) => {
-                        browser.elementIdText(elementId, function(words) {
+                        browser.elementIdText(elementId, (words) => {
                             if (previousWC === 0) {
                                 previousWC = parseInt(words.value);
                             } else {
@@ -44,19 +44,19 @@ module.exports = {
             .click('#sort-by-test_Pressbooks_directory')
             .pause(3000)
             .waitForElementVisible('.ais-Hits__books-book-wordcount')
-            .elements('css selector', '.ais-Hits__books-book', function(bookElement) {
+            .elements('css selector', '.ais-Hits__books-book', (bookElement) => {
                 let previousName = '', elementId;
                 bookElement.value.forEach((v) => {
                     if (!v.hasOwnProperty('ELEMENT')) {
                         v.ELEMENT = Object.values(v)[0];
                     }
-                    browser.elementIdElement(v.ELEMENT, 'css selector', '.ais-Hits__books .v-card__title', function (elem) {
+                    browser.elementIdElement(v.ELEMENT, 'css selector', '.ais-Hits__books .v-card__title', (elem) => {
                         if (!elem.value.hasOwnProperty('ELEMENT')) {
                             elem.value.ELEMENT = Object.values(elem.value)[0];
                         }
                         elementId = elem.value.ELEMENT;
                     }).perform((done) => {
-                        browser.elementIdText(elementId, function(words) {
+                        browser.elementIdText(elementId, (words) => {
                             words.value = words.value.toLowerCase();
                             if (previousName === '') {
                                 previousName = words.value;
@@ -80,19 +80,19 @@ module.exports = {
             .click('#sort-by-test_Pressbooks_directory_sort_by_lastUpdated')
             .pause(3000)
             .waitForElementVisible('.updated')
-            .elements('css selector', '.ais-Hits__books-book', function(bookElement) {
+            .elements('css selector', '.ais-Hits__books-book', (bookElement) => {
                 let previousDate = 0, elementId;
                 bookElement.value.forEach((v) => {
                     if (!v.hasOwnProperty('ELEMENT')) {
                         v.ELEMENT = Object.values(v)[0];
                     }
-                    browser.elementIdElement(v.ELEMENT, 'css selector', '.updated', function (elem) {
+                    browser.elementIdElement(v.ELEMENT, 'css selector', '.updated', (elem) => {
                         if (!elem.value.hasOwnProperty('ELEMENT')) {
                             elem.value.ELEMENT = Object.values(elem.value)[0];
                         }
                         elementId = elem.value.ELEMENT;
                     }).perform((done) => {
-                        browser.elementIdText(elementId, function(date) {
+                        browser.elementIdText(elementId, (date) => {
                             if (previousDate === 0) {
                                 previousDate = new Date(date.value);
                             } else {
