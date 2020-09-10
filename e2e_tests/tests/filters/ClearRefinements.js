6d43a412d3d54f2b0419be76f1c69066f1ef96cf
ricardopressbooks <63249561+ricardopressbooks@users.noreply.github.com>
2020-09-10 18:34:21 -0300

    Fixing e2e tests for clear refinements action (#101)
    * Fix e2e tests for clear refinements action

--- e2e_tests/tests/filters/ClearRefinements.js
+++ e2e_tests/tests/filters/ClearRefinements.js
@@ -3,13 +3,33 @@ module.exports = {
     browser
       .url(process.env.HOST_TEST)
       .waitForElementVisible('body')
-      .waitForElementVisible('#filter-license_code')
-      .click('#btn-exclude-license_code-All-Rights-Reserved')
-      .waitForElementVisible('.v-chip--clickable')
-      .pause(2000)
-      .click('.ais-ClearRefinements-button')
-      .pause(2000)
-      .assert.cssClassPresent('.ais-Hits__books-book', 'ais-Hits__books--allrights')
-      .end();
+      .pause(3000)
+      .waitForElementVisible('.container__results-hits')
+      .getText('css selector', '.container__results-hits',  (d) => {
+        let text = d.value.split(' ');
+        let shown = parseInt(text[2]);
+        browser.element('css selector', '#filter-networkName', (filterElement) => {
+          // Firefox - Safari exception
+          if (!filterElement.hasOwnProperty('ELEMENT')) {
+            filterElement.ELEMENT = Object.values(filterElement.value)[0];
+          }
+          browser.elementIdElements(filterElement.ELEMENT, 'css selector', '.v-btn--icon',  (button) => {
+            if (!button.value[0].hasOwnProperty('ELEMENT')) {
+              button.value[0].ELEMENT = Object.values(button.value[0])[0];
+            }
+            browser.elementIdClick(button.value[0].ELEMENT,  () => {
+              browser
+                .pause(2000)
+                .click('.ais-ClearRefinements-button')
+                .pause(3000)
+                .getText('css selector', '.container__results-hits',  (d) => {
+                  let text2 = d.value.split(' ');
+                  let shown2 = parseInt(text2[2]);
+                  browser.assert.ok(shown === shown2, 'Books total is the same after clearing all filters');
+                }).end();
+            });
+          });
+        });
+      });
   }
 };
