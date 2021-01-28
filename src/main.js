35860331a0a1c2317320b4e3b819277949e6c9d7
Ricardo Aragon <richard015ar@gmail.com>
2021-01-28 17:51:45 -0500

    Add vue-meta for meta tag management. Add robots.txt and exclude it from production environment

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import { store } from './store/index';
 import router from './router';
 import * as Sentry from '@sentry/vue';
 import { Integrations } from '@sentry/tracing';
+import VueMeta from 'vue-meta';
 
+Vue.use(VueMeta);
 Vue.use(InstantSearch);
 Vue.config.productionTip = false;
 
