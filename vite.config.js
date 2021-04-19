4a010b49ba3f525720b2cc4a6d80795e5de4ebba
SteelWagstaff <steel@pressbooks.com>
2021-04-19 11:12:09 -0700

    Changes by arzola

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { createVuePlugin } from 'vite-plugin-vue2';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()]
-})
+module.exports = {
+  plugins: [createVuePlugin(/*options*/)],
+};
