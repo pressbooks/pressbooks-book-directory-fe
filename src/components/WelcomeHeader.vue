cfd0f3976123bc99c77854a6df6dca56bf16fdf9
SteelWagstaff <steel@pressbooks.com>
2021-04-19 09:09:03 -0700

    Improve text

--- src/components/WelcomeHeader.vue
+++ src/components/WelcomeHeader.vue
@@ -4,11 +4,10 @@
       <h1 class="section-title text-5xl mb-6">
         Welcome to Pressbooks Directory
       </h1>
-      <p class="section-summary font-pbRegular text-gray-700 mb-8 leading-7 text-center">
+      <p class="section-summary font-pbRegular text-gray-700 mb-8 leading-7">
         This directory provides an index of <strong>8888</strong> books published across
         <strong>u878</strong> Pressbooks networks. Learn more about how the directory
-        works by <a href="https://networkmanagerguide.pressbooks.com/chapter/how-to-use-the-pressbooks-directory/">reading our guide</a>
-        or taking the self-guided tour below.
+        works by taking the self-guided tour in the top nav bar or <a href="https://networkmanagerguide.pressbooks.com/chapter/how-to-use-the-pressbooks-directory/">reading our guide</a>.
       </p>
     </div>
   </section>
