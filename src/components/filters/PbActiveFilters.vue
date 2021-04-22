d0e54e009368044fcdc8c11fc8c562cef77399e7
Ricardo Aragon <richard015ar@gmail.com>
2021-04-22 08:24:11 -0400

    Add chips for remove/add filters

--- src/components/filters/PbActiveFilters.vue
+++ src/components/filters/PbActiveFilters.vue
@@ -3,8 +3,20 @@
     <div class="font-semibold text-lg mb-3">
       Active Filters
     </div>
-    <div class="active-filters flex flex-row flex-wrap">
-      <pb-selected-filter name="Education" />
+    <div v-if="Object.keys($store.state.SClient.filtersExcluded).length > 0">
+      <div
+        v-for="iv in $store.state.SClient.filtersExcluded"
+        class="active-filters flex flex-row flex-wrap"
+      >
+        <pb-selected-filter
+          v-for="value in iv"
+          :key="value.value"
+          :name="getLabel(value)"
+          :value="value"
+          :attributes-allowed="attrAllowed"
+          :attributes-allow-empty="attrAllowedEmpty"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -15,6 +27,66 @@ export default {
   name: 'PbActiveFilters',
   components: {
     PbSelectedFilter
+  },
+  data() {
+    return {
+      attrAllowed: [],
+      allowed: this.$store.state.SClient.allowedFilters,
+      attrAllowedEmpty: []
+    };
+  },
+  mounted() {
+    this. attrAllowed = Object.keys(this.$store.state.SClient.allowedFilters);
+    let allowed = this.allowed;
+    this.attrAllowedEmpty = Object.keys(this.allowed).map(key => {
+      if (typeof allowed[key].empty !== 'undefined') {
+        return allowed[key].empty;
+      }
+    });
+  },
+  methods: {
+    inputFormatDate(d) {
+      let month = (d.getUTCMonth()+1) < 10 ? '0' + (d.getUTCMonth()+1) : (d.getUTCMonth()+1);
+      let day = d.getUTCDate() < 10 ? '0' + d.getUTCDate() : d.getUTCDate();
+      return month + '/' + day + '/' + d.getUTCFullYear();
+    },
+    removeFilters() {
+      this.$router.replace({ query: {} });
+    },
+    getLabel(value) {
+      let label;
+      let mb = (value.attribute === 'storageSize') ? parseInt(value.value) : 0;
+      switch (value.attribute) {
+      case 'hasIsBasedOn':
+        label = (value.value) ? 'Based on another book' : 'Original';
+        break;
+      case 'isRecommended':
+        label = (value.value) ? 'Recommended' : 'Not recommended';
+        break;
+      case 'wordCount':
+        label = 'Words ' + value.operator + ' ' + value.value;
+        break;
+      case 'lastUpdated':
+        let dateObj = new Date(parseInt(value.value) * 1000);
+        let date = this.inputFormatDate(dateObj);
+        label = 'Updated ' + value.operator + ' ' + date;
+        break;
+      case 'storageSize':
+        label = 'Storage ' + value.operator + ' ' + parseFloat(mb).toFixed(2) + ' MB';
+        break;
+      case 'h5pActivities':
+        label = 'H5P Activities ' + value.operator + ' ' + value.value;
+        break;
+      default:
+        if (this.attrAllowedEmpty.indexOf(value.attribute) >= 0) {
+          let aliasType = this.getAliasType(value);
+          label = aliasType.alias + ': Empty';
+        } else {
+          label = value.value;
+        }
+      }
+      return (value.exclude) ? 'NOT ' + label : label;
+    }
   }
 };
 </script>
--- src/components/filters/PbSelectedFilter.vue
+++ src/components/filters/PbSelectedFilter.vue
@@ -4,7 +4,10 @@
       {{ name }}
     </div>
     <div>
-      <button class="text block ml-2">
+      <button
+        @click.prevent="closeExcludeFilter(value)"
+        class="text block ml-2"
+      >
         x
       </button>
     </div>
@@ -18,6 +21,68 @@ export default {
     name: {
       type: String,
       default: ''
+    },
+    value: {
+      type: Object,
+      default() { return {}; }
+    },
+    attributesAllowed: {
+      type: Array,
+      default() { return []; }
+    },
+    attributesAllowEmpty: {
+      type: Array,
+      default() { return []; }
+    }
+  },
+  methods: {
+    getAliasType(f) {
+      for (let attribute in this.$store.state.SClient.allowedFilters) {
+        if (f.attribute === attribute) {
+          return {
+            alias: this.$store.state.SClient.allowedFilters[attribute].alias,
+            type: this.$store.state.SClient.allowedFilters[attribute].type
+          };
+        }
+        if (f.attribute === this.$store.state.SClient.allowedFilters[attribute].empty) {
+          return {
+            alias: this.$store.state.SClient.allowedFilters[attribute].alias,
+            type: this.$store.state.SClient.allowedFilters[attribute].type
+          };
+        }
+      }
+      return false;
+    },
+    closeExcludeFilter(f) {
+      let currentQuery = {...this.$route.query};
+      let attrAllowed = this.attributesAllowed.concat(this.attributesAllowEmpty);
+      let aliasType = this.getAliasType(f);
+      if (attrAllowed.indexOf(f.attribute) < 0 || !aliasType) {
+        return false;
+      }
+      for (let attr in currentQuery) {
+        if (attr === aliasType.alias) {
+          currentQuery[attr] = currentQuery[attr].split('&&');
+          for (let i = 0; i < currentQuery[attr].length; i++) {
+            let value = (aliasType.type === 'numeric') ?
+              currentQuery[attr][i].substr(2) : currentQuery[attr][i];
+            if (aliasType.type === 'boolean') {
+              value = (currentQuery[attr][i] === 'true').toString();
+            } else if(f.exclude) {
+              value = currentQuery[attr][i].substr(1);
+            }
+            if (f.value === value || value === 'empty') {
+              currentQuery[attr].splice(i, 1);
+              if (currentQuery[attr].length === 0) {
+                delete currentQuery[attr];
+              } else {
+                currentQuery[attr] = currentQuery[attr].join('&&');
+              }
+              return this.$router.replace({ query: currentQuery });
+            }
+          }
+        }
+      }
     }
   }
 };
