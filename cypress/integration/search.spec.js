c6f0bf38b7c6f4ff9a882b6f333f91d21b2ad59b
arzola <oscararzola@gmail.com>
2021-04-27 16:20:01 -0500

    e2e integration with Cypress, browserstack

--- cypress/integration/search.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Search', () => {
-  it('Search for specific books', () => {
-    cy.visit('/');
-    cy.get('div.input input').type('music');
-    cy.get('form button[type="submit"]').click();
-    cy.url()
-      .should('include','?q=music');
-  });
-});
