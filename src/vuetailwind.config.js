a6cee50737dbb5b5a08e604d19927a4af7415466
Felipe Dalcin <felipe@pressbooks.com>
2021-05-05 14:26:46 -0300

    Finalizing styling

--- src/vuetailwind.config.js
+++ src/vuetailwind.config.js
@@ -22,7 +22,7 @@ export default {
         calendarHeaderWrapper: 'grid grid-cols-7',
         monthWrapper: 'grid grid-cols-4',
         yearWrapper: 'grid grid-cols-4',
-        input: 'block w-full px-3 py-2 transition duration-100 ease-in-out border rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
+        input: 'block w-full py-2 px-3 border-0 border-b focus:border-red-700 focus:outline-none focus:ring-0 disabled:opacity-50 disabled:cursor-not-allowed',
         clearButton: 'flex flex-shrink-0 items-center justify-center absolute right-0 top-0 m-2 h-6 w-6',
         clearButtonIcon: 'fill-current h-3 w-3'
       },
@@ -37,9 +37,9 @@ export default {
         leaveActiveClass: 'transition transform ease-in duration-75',
         leaveToClass: 'opacity-0 scale-95',
         inlineWrapper: '',
-        inlineViews: 'rounded bg-white border mt-1 inline-flex w-full',
+        inlineViews: 'rounded bg-white border-0 inline-flex w-full',
         inputWrapper: '',
-        input: 'text-black placeholder-gray-400 border-gray-300',
+        input: 'text-gray-900 placeholder-gray-400 border-gray-300',
         clearButton: 'hover:bg-gray-100 rounded transition duration-100 ease-in-out text-gray-600',
         clearButtonIcon: '',
         viewGroup: '',
@@ -63,29 +63,29 @@ export default {
         calendarHeaderWeekDay: 'uppercase text-xs text-gray-500 w-8 h-8 flex items-center justify-center',
         calendarDaysWrapper: '',
         calendarDaysDayWrapper: 'w-full h-8 flex flex-shrink-0 items-center',
-        otherMonthDay: 'text-sm rounded-full w-8 h-8 mx-auto hover:bg-blue-100 text-gray-400 disabled:opacity-50 disabled:cursor-not-allowed',
+        otherMonthDay: 'text-sm rounded-full w-8 h-8 mx-auto hover:bg-red-100 text-gray-400 disabled:opacity-50 disabled:cursor-not-allowed',
         emptyDay: '',
-        inRangeFirstDay: 'text-sm bg-blue-500 text-white w-full h-8 rounded-l-full',
-        inRangeLastDay: 'text-sm bg-blue-500 text-white w-full h-8 rounded-r-full',
-        inRangeDay: 'text-sm bg-blue-200 w-full h-8 disabled:opacity-50 disabled:cursor-not-allowed',
-        selectedDay: 'text-sm rounded-full w-8 h-8 mx-auto bg-blue-500 text-white disabled:opacity-50 disabled:cursor-not-allowed',
-        activeDay: 'text-sm rounded-full bg-blue-100 w-8 h-8 mx-auto disabled:opacity-50 disabled:cursor-not-allowed',
-        highlightedDay: 'text-sm rounded-full bg-blue-200 w-8 h-8 mx-auto disabled:opacity-50 disabled:cursor-not-allowed',
-        day: 'text-sm rounded-full w-8 h-8 mx-auto hover:bg-blue-100 disabled:opacity-50 disabled:cursor-not-allowed',
-        today: 'text-sm rounded-full w-8 h-8 mx-auto hover:bg-blue-100 disabled:opacity-50 disabled:cursor-not-allowed border border-blue-500',
+        inRangeFirstDay: 'text-sm bg-red-700 text-white w-full h-8 rounded-l-full',
+        inRangeLastDay: 'text-sm bg-red-700 text-white w-full h-8 rounded-r-full',
+        inRangeDay: 'text-sm bg-red-200 w-full h-8 disabled:opacity-50 disabled:cursor-not-allowed',
+        selectedDay: 'text-sm rounded-full w-8 h-8 mx-auto bg-red-700 text-white disabled:opacity-50 disabled:cursor-not-allowed',
+        activeDay: 'text-sm rounded-full bg-red-100 w-8 h-8 mx-auto disabled:opacity-50 disabled:cursor-not-allowed',
+        highlightedDay: 'text-sm rounded-full bg-red-200 w-8 h-8 mx-auto disabled:opacity-50 disabled:cursor-not-allowed',
+        day: 'text-sm rounded-full w-8 h-8 mx-auto hover:bg-red-100 disabled:opacity-50 disabled:cursor-not-allowed',
+        today: 'text-sm rounded-full w-8 h-8 mx-auto hover:bg-red-100 disabled:opacity-50 disabled:cursor-not-allowed border border-red-700',
         monthWrapper: 'px-3 pt-2',
-        selectedMonth: 'text-sm rounded w-full h-12 mx-auto bg-blue-500 text-white',
-        activeMonth: 'text-sm rounded w-full h-12 mx-auto bg-blue-100',
-        month: 'text-sm rounded w-full h-12 mx-auto hover:bg-blue-100',
+        selectedMonth: 'text-sm rounded w-full h-12 mx-auto bg-red-700 text-white',
+        activeMonth: 'text-sm rounded w-full h-12 mx-auto bg-red-100',
+        month: 'text-sm rounded w-full h-12 mx-auto hover:bg-red-100',
         yearWrapper: 'px-3 pt-2',
-        year: 'text-sm rounded w-full h-12 mx-auto hover:bg-blue-100',
-        selectedYear: 'text-sm rounded w-full h-12 mx-auto bg-blue-500 text-white',
-        activeYear: 'text-sm rounded w-full h-12 mx-auto bg-blue-100'
+        year: 'text-sm rounded w-full h-12 mx-auto hover:bg-red-100',
+        selectedYear: 'text-sm rounded w-full h-12 mx-auto bg-red-700 text-white',
+        activeYear: 'text-sm rounded w-full h-12 mx-auto bg-red-100'
       },
       variants: {
         danger: {
           input: 'border-red-300 bg-red-50 placeholder-red-200 text-red-900',
-          clearButton: 'hover:bg-red-200 text-red-500'
+          clearButton: 'hover:bg-red-200 text-red-700'
         }
       }
     }
@@ -93,13 +93,13 @@ export default {
   't-button': {
     component: TButton,
     props: {
-      fixedClasses: 'block px-4 py-2 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
-      classes: 'text-white bg-blue-500 border border-transparent shadow-sm rounded hover:bg-blue-600',
+      fixedClasses: 'block px-4 py-2 font-bold focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed',
+      classes: 'text-white bg-red-700 shadow-sm rounded hover:bg-red-700',
       variants: {
         secondary: 'text-gray-800 bg-white border border-gray-300 shadow-sm hover:text-gray-600',
-        error: 'text-white bg-red-500 border border-transparent rounded shadow-sm hover:bg-red-600',
+        error: 'text-white bg-red-700 border border-transparent rounded shadow-sm hover:bg-red-700',
         success: 'text-white bg-green-500 border border-transparent rounded shadow-sm hover:bg-green-600',
-        link: 'text-blue-500 underline hover:text-blue-600'
+        link: 'text-red-700 underline hover:text-red-600'
       }
     }
   }
