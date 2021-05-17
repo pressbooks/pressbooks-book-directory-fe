8d6475f5973e96131c5c7d085c175c0313360c0f
SteelWagstaff <steel@pressbooks.com>
2021-05-17 07:32:26 -0700

    Add tests and linting fixes

--- e2e/integration/externalLinks.spec.js
+++ e2e/integration/externalLinks.spec.js
@@ -1,9 +1,14 @@
 describe('External links', () => {
   context('Desktop resolution', () => {
     beforeEach(() => {
-
       cy.get('[data-cy=learn-about-pressbooks]').as('learnAboutPressbooks');
       cy.get('[data-cy=pressbooks-homepage]').as('pressbooksHomepage');
+      cy.get('[data-cy=pressbooks-blog]').as('pressbooksBlog');
+      cy.get('[data-cy=pressbooks-opensource]').as('pressbooksOpensource');
+      cy.get('[data-cy=pressbooks-contact]').as('pressbooksContact');
+      cy.get('[data-cy=pressbooks-jobs]').as('pressbooksJobs');
+      cy.get('[data-cy=pressbooks-accessibility]').as('pressbooksAccessibility');
+      cy.get('[data-cy=pressbooks-procurement]').as('pressbooksProcurement');
       cy.get('[data-cy=twitter-social-link]').as('twitterProfile');
       cy.get('[data-cy=linkedin-social-link]').as('linkedinProfile');
       cy.get('[data-cy=youtube-social-link]').as('youtubeProfile');
@@ -14,7 +19,7 @@ describe('External links', () => {
         .should('have.attr', 'href', 'https://pressbooks.com')
         .should('have.attr', 'rel', 'noopener')
         .then((link) => {
-          cy.request(link.prop('href')).its('status').should('eq', 200)
+          cy.request(link.prop('href')).its('status').should('eq', 200);
         });
     });
 
@@ -23,7 +28,46 @@ describe('External links', () => {
         .should('have.attr', 'href', 'https://pressbooks.com')
         .should('have.attr', 'rel', 'noopener')
         .then((link) => {
-          cy.request(link.prop('href')).its('status').should('eq', 200)
+          cy.request(link.prop('href')).its('status').should('eq', 200);
+        });
+    });
+
+    it('Footer has no broken links', () => {
+      cy.get('@pressbooksBlog')
+        .should('have.attr', 'href', 'https://pressbooks.com/blog')
+        .should('have.attr', 'rel', 'noopener')
+        .then( (link ) => {
+          cy.request(link.prop('href')).its('status').should('eq', 200);
+        });
+      cy.get('@pressbooksOpensource')
+        .should('have.attr', 'href', 'https://pressbooks.org')
+        .should('have.attr', 'rel', 'noopener')
+        .then( (link ) => {
+          cy.request(link.prop('href')).its('status').should('eq', 200);
+        });
+      cy.get('@pressbooksContact')
+        .should('have.attr', 'href', 'https://pressbooks.com/contact-pressbooks/')
+        .should('have.attr', 'rel', 'noopener')
+        .then( (link ) => {
+          cy.request(link.prop('href')).its('status').should('eq', 200);
+        });
+      cy.get('@pressbooksJobs')
+        .should('have.attr', 'href', 'https://pressbooks.com/about/#work-with-us')
+        .should('have.attr', 'rel', 'noopener')
+        .then( (link ) => {
+          cy.request(link.prop('href')).its('status').should('eq', 200);
+        });
+      cy.get('@pressbooksAccessibility')
+        .should('have.attr', 'href', 'https://pressbooks.com/accessibility/')
+        .should('have.attr', 'rel', 'noopener')
+        .then((link) => {
+          cy.request(link.prop('href')).its('status').should('eq', 200);
+        });
+      cy.get('@pressbooksProcurement')
+        .should('have.attr', 'href', 'https://pressbooks.com/edu-procurement-helpers/')
+        .should('have.attr', 'rel', 'noopener')
+        .then((link) => {
+          cy.request(link.prop('href')).its('status').should('eq', 200);
         });
     });
 
--- src/components/PbFooter.vue
+++ src/components/PbFooter.vue
@@ -5,77 +5,77 @@
         <ul class="flex flex-col font-bold lg:flex-row w-full items-center justify-between space-y-8 lg:space-y-0">
           <li class="nav-item">
             <a
-                :href="site"
-                aria-label="Pressbooks Homepage"
-                target="_blank"
-                rel="noopener"
-                data-cy="pressbooks-homepage"
+              :href="site"
+              aria-label="Pressbooks Homepage"
+              target="_blank"
+              rel="noopener"
+              data-cy="pressbooks-homepage"
             >
               Pressbooks
             </a>
           </li>
           <li class="nav-item">
             <a
-                :href="blog"
-                aria-label="Pressbooks blog"
-                target="_blank"
-                rel="noopener"
-                data-cy="pressbooks-blog"
+              :href="blog"
+              aria-label="Pressbooks blog"
+              target="_blank"
+              rel="noopener"
+              data-cy="pressbooks-blog"
             >
               Blog
             </a>
           </li>
           <li class="nav-item">
             <a
-                :href="opensource"
-                aria-label="Pressbooks Open Source"
-                target="_blank"
-                rel="noopener"
-                data-cy="pressbooks-opensource"
+              :href="opensource"
+              aria-label="Pressbooks Open Source"
+              target="_blank"
+              rel="noopener"
+              data-cy="pressbooks-opensource"
             >
               Pressbooks Open Source
             </a>
           </li>
           <li class="nav-item">
             <a
-                :href="contact"
-                aria-label="Contact"
-                target="_blank"
-                rel="noopener"
-                data-cy="pressbooks-contact"
+              :href="contact"
+              aria-label="Contact"
+              target="_blank"
+              rel="noopener"
+              data-cy="pressbooks-contact"
             >
               Contact
             </a>
           </li>
           <li class="nav-item">
             <a
-                :href="jobs"
-                aria-label="Work with us"
-                target="_blank"
-                rel="noopener"
-                data-cy="pressbooks-jobs"
+              :href="jobs"
+              aria-label="Work with us"
+              target="_blank"
+              rel="noopener"
+              data-cy="pressbooks-jobs"
             >
               Work with us
             </a>
           </li>
           <li class="nav-item">
             <a
-                :href="accessibility"
-                aria-label="Accessibility"
-                target="_blank"
-                rel="noopener"
-                data-cy="pressbooks-accessibility"
+              :href="accessibility"
+              aria-label="Accessibility"
+              target="_blank"
+              rel="noopener"
+              data-cy="pressbooks-accessibility"
             >
               Accessibility
             </a>
           </li>
           <li class="nav-item">
             <a
-                :href="procurement"
-                aria-label="EDU Procurement Helpers"
-                target="_blank"
-                rel="noopener"
-                data-cy="pressbooks-procurement"
+              :href="procurement"
+              aria-label="EDU Procurement Helpers"
+              target="_blank"
+              rel="noopener"
+              data-cy="pressbooks-procurement"
             >
               EDU Procurement Helpers
             </a>
@@ -89,11 +89,11 @@
         <ais-powered-by class="inline-block" />
         <div class="flex space-x-4">
           <pb-social-link
-              v-for="(socialLink, key) in socialLinks"
-              :key="key"
-              :href="socialLink.href"
-              :icon="socialLink.icon"
-              :name="socialLink.name"
+            v-for="(socialLink, key) in socialLinks"
+            :key="key"
+            :href="socialLink.href"
+            :icon="socialLink.icon"
+            :name="socialLink.name"
           />
         </div>
       </div>
