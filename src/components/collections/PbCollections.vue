54be7b3ec67f4c3029bd6bcafc70b731cbb1f576
SteelWagstaff <steel@pressbooks.com>
2021-06-04 15:11:19 -0700

    Change 'Featured' to 'Curated' Collections

--- src/components/collections/PbCollections.vue
+++ src/components/collections/PbCollections.vue
@@ -8,7 +8,7 @@
       <div class="border-gray-300">
         <div class="w-full mx-auto text-center">
           <h2 class="section-title font-bold text-4xl mb-12">
-            Featured Collections
+            Curated Collections
           </h2>
           <div class="grid grid-cols-1 gap-y-3 md:grid-cols-5 md:gap-x-3">
             <pb-collection-card
