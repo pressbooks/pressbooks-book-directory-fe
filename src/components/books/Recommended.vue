b656c8017554a415784e6de347546aae8b66a3f5
SteelWagstaff <steel@pressbooks.com>
2021-06-04 16:46:30 -0700

    More CSS changes

--- src/components/books/Recommended.vue
+++ src/components/books/Recommended.vue
@@ -2,7 +2,7 @@
   <button
     type="button"
     data-cy="book-recommended"
-    class="inline-block leading-none border border-red-900 py-2 px-3 text-sm font-bold rounded mb-5 mr-2"
+    class="inline-block leading-none border border-red-900 py-2 px-3 text-sm font-bold rounded mb-2 mr-2"
     :class="enabled ? 'bg-red-700 text-white': 'text-pb-red border-pb-red'"
     @click.prevent="toggle('recommended')"
   >
