bce935bb8bdaa653a7bc923dee71ced191467b44
arzola <oscararzola@gmail.com>
2021-07-06 14:20:56 -0500

    Image fallback

--- src/components/PbBookCover.vue
+++ src/components/PbBookCover.vue
@@ -1,18 +1,23 @@
 <template>
   <img
+    v-img-fallback="'/assets/images/default-book-cover.jpg'"
     class="w-full"
     :src="image"
   >
 </template>
 
 <script>
+import { ImgFallback } from 'v-img-fallback';
 export default {
   name: 'PbBookCover',
+  directives: {
+    ImgFallback
+  },
   props: {
     image: {
       type: String,
       default: ''
     },
-  }
+  },
 };
 </script>
