950b2726db41918f247d7db56f1e94d51873bb77
Felipe Dalcin <felipe@pressbooks.com>
2023-05-24 14:12:07 -0400

    chore: bump search-insights library

--- src/mixins/AlgoliaMixin.js
+++ src/mixins/AlgoliaMixin.js
@@ -3,6 +3,11 @@ import searchInsights from 'search-insights';
 export default {
   // this will set the user token required by algolia
   beforeMount() {
+    searchInsights('init', {
+      appId: import.meta.env.VITE_ALGOLIA_APP_ID,
+      apiKey: import.meta.env.VITE_ALGOLIA_API_READ_KEY,
+    });
+
     searchInsights('setUserToken', 'pressbooks-directory');
   },
   methods: {
