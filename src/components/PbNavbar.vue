20ce0efdb1bc1c8a3dbac366577e8e36342147eb
Oscar Arzola <oscararzola@gmail.com>
2024-09-26 14:09:08 -0400

    fix: accesibility improvements

--- src/components/PbNavbar.vue
+++ src/components/PbNavbar.vue
@@ -48,10 +48,20 @@
           class="hidden w-full lg:block lg:w-auto"
         >
           <ul class="font-medium flex items-start lg:items-center gap-4 flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:mt-0 lg:border-0 lg:bg-white">
-            <li class="group relative">
+            <li
+              class="group relative"
+              role="menu"
+              @mouseleave="hideSubmenu('products')"
+            >
               <a
                 href="#"
+                aria-haspopup="true"
+                aria-expanded="false"
                 class="flex gap-0.5 items-center py-2 pl-3 pr-4 text-pb-dark-blue rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-pb-red lg:p-0 font-semibold text-[18px]"
+                @keydown.enter.prevent="toggleSubmenu('products')"
+                @keydown.space.prevent="toggleSubmenu('products')"
+                @focus="showSubmenu('products')"
+                @mouseover="showSubmenu('products')"
               >
                 Products
                 <span class="ml-1">
@@ -70,32 +80,47 @@
                   </svg>
                 </span>
               </a>
-              <!-- Dropdown menu -->
-              <ul class="absolute hidden group-hover:block bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60">
-                <li>
+              <ul
+                v-show="isSubmenuVisible('products')"
+                id="products"
+                ref="products"
+                class="absolute bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60"
+                role="menu"
+              >
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/enterprise/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Enterprise</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/self-publisher/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Self-Publisher</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="/"
                     class="text-gray-500 font-semibold text-[18px]"
+                    @blur="hideSubmenu('products')"
                   >Pressbooks Directory</a>
                 </li>
               </ul>
             </li>
-            <li class="group relative">
+            <li
+              class="group relative"
+              @mouseleave="hideSubmenu('plans')"
+            >
               <a
+                aria-haspopup="true"
+                aria-expanded="false"
                 href="https://pressbooks.com/plans-pricing/"
                 class="flex gap-0.5 items-center py-2 pl-3 pr-4 text-pb-dark-blue rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-pb-red lg:p-0 font-semibold text-[18px]"
+                @keydown.enter.prevent="toggleSubmenu('plans')"
+                @keydown.space.prevent="toggleSubmenu('plans')"
+                @focus="showSubmenu('plans')"
+                @mouseover="showSubmenu('plans')"
               >
                 Plans & Pricing
                 <span class="ml-1">
@@ -114,25 +139,41 @@
                   </svg>
                 </span>
               </a>
-              <ul class="absolute hidden group-hover:block bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60">
-                <li>
+              <ul
+                v-show="isSubmenuVisible('plans')"
+                id="plans"
+                ref="plans"
+                class="absolute group-hover:block bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60"
+                role="menu"
+              >
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/educational-institutions-plans-and-pricing/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Enterprise Networks</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/self-publisher-plans-and-pricing/"
                     class="text-gray-500 font-semibold text-[18px]"
+                    @blur="hideSubmenu('plans')"
                   >Self-Publisher Plans</a>
                 </li>
               </ul>
             </li>
-            <li class="group relative">
+            <li
+              class="group relative"
+              @mouseleave="hideSubmenu('resources')"
+            >
               <a
+                aria-haspopup="true"
+                aria-expanded="false"
                 href="#"
                 class="flex gap-0.5 items-center py-2 pl-3 pr-4 text-pb-dark-blue rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-pb-red lg:p-0 font-semibold text-[18px]"
+                @keydown.enter.prevent="toggleSubmenu('resources')"
+                @keydown.space.prevent="toggleSubmenu('resources')"
+                @focus="showSubmenu('resources')"
+                @mouseover="showSubmenu('resources')"
               >
                 Resources
                 <span class="ml-1">
@@ -151,49 +192,65 @@
                   </svg>
                 </span>
               </a>
-              <ul class="absolute hidden group-hover:block bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60">
-                <li>
+              <ul
+                v-show="isSubmenuVisible('resources')"
+                id="resources"
+                ref="resources"
+                class="absolute group-hover:block bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60"
+                role="menu"
+              >
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/collections-hub/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Collections Hub</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/category/success-stories/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Success Stories</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/pressbooks-blog/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Blog</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Pressbooks Directory</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/support/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Support Guides</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/webinars/"
                     class="text-gray-500 font-semibold text-[18px]"
+                    @blur="hideSubmenu('resources')"
                   >Events & Webinars</a>
                 </li>
               </ul>
             </li>
-            <li class="group relative">
+            <li
+              class="group relative"
+              @mouseleave="hideSubmenu('about')"
+            >
               <a
+                aria-haspopup="true"
+                aria-expanded="false"
                 href="https://pressbooks.com/about/"
                 class="flex gap-0.5 items-center py-2 pl-3 pr-4 text-pb-dark-blue rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-pb-red lg:p-0 font-semibold text-[18px]"
+                @keydown.enter.prevent="toggleSubmenu('about')"
+                @keydown.space.prevent="toggleSubmenu('about')"
+                @focus="showSubmenu('about')"
+                @mouseover="showSubmenu('about')"
               >
                 About
                 <span class="ml-1">
@@ -212,27 +269,35 @@
                   </svg>
                 </span>
               </a>
-              <ul class="absolute hidden group-hover:block bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60">
-                <li>
+              <ul
+                v-show="isSubmenuVisible('about')"
+                id="about"
+                ref="about"
+                class="absolute group-hover:block bg-white shadow-lg -mt-1 rounded-lg p-4 space-y-2 z-50 min-w-60"
+                role="menu"
+              >
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/about/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >About Pressbooks</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/pressbooks-team/"
                     class="text-gray-500 font-semibold text-[18px]"
                   >Our Team</a>
                 </li>
-                <li>
+                <li role="menuitem">
                   <a
                     href="https://pressbooks.com/category/inside-pressbooks/"
                     class="text-gray-500 font-semibold text-[18px]"
+                    @blur="hideSubmenu('about')"
                   >Inside Pressbooks</a>
                 </li>
               </ul>
             </li>
+            <!-- Request a Demo Button -->
             <li class="lg:inline-block lg:text-center lg:border-2 lg:border-red-700 lg:text-white lg:bg-red-700 lg:rounded-full lg:leading-[20px] lg:ml-24">
               <a
                 href="https://pressbooks.com/request-a-pressbooks-demo/"
@@ -246,9 +311,33 @@
   </header>
 </template>
 
-<script>
-import {Collapse} from 'flowbite';
+<script setup>
+import {ref} from 'vue';
 
+const submenus = ref({
+  products: false,
+  plans: false,
+  resources: false,
+  about: false
+});
+
+const isSubmenuVisible = (id) => {
+  return submenus.value[id];
+};
+
+const showSubmenu = (id) => {
+  submenus.value[id] = true;
+};
+
+const hideSubmenu = (id) => {
+  submenus.value[id] = false;
+};
+
+const toggleSubmenu = (id) => {
+  submenus.value[id] = !submenus.value[id];
+};
+
+import {Collapse} from 'flowbite';
 const $targetEl = document.getElementById('navbar-top');
 const $triggerEl = document.getElementById('toggle-navbar-top');
 new Collapse($targetEl, $triggerEl);
